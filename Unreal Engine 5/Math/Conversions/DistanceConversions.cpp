bbb993aeb5f366ffe9d3e050b1635dc18b9c67f7
Lillian Martin <127252051+LilithRosa@users.noreply.github.com>
2026-01-12 18:37:17 -0800

    Create DistanceConversions.cpp

---
+++ Unreal Engine 5/Math/Conversions/DistanceConversions.cpp
@@ -0,0 +1,13 @@
+// Unreal Engine 5 Distance Conversion Example cpp class file 
+
+#include "DistanceConversions.h"
+
+double UDistanceConversions::ToMeters(EDistanceUnit Unit)
+{
+     switch (Unit)
+     {
+
+     }
+}
+
+// EOF
