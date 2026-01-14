4092d95117b67c6aed3ad8441743203fd7635ea9
Lillian Martin <127252051+LilithRosa@users.noreply.github.com>
2026-01-13 16:26:16 -0800

    Create Tutorials/Crypto/SHA256/SHA256.h
    working on

---
+++ Tutorials/Crypto/SHA256/SHA256.h
@@ -0,0 +1,21 @@
+// Basic SHA-256 Algo as defined in NIST FIPS 180-4.
+
+
+#include <cstsint>
+#include <cstdlib>
+
+class SHA256
+{
+
+public:
+
+      CSHA256();
+      CSHA256& Write(const unsigned char*);
+
+
+private:
+
+};
+
+
+// EOF
