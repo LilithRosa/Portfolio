e86c41e10d1e247ec91d6169163676fd1f8e4c63
Lillian Martin <127252051+LilithRosa@users.noreply.github.com>
2026-01-15 20:29:38 -0800

    Update Basic10Key.cpp

--- Unreal Engine 5/Slate/UI/Basic10Key.cpp
+++ Unreal Engine 5/Slate/UI/Basic10Key.cpp
@@ -24,16 +24,22 @@ TSharedRef<SWidget> UBasic10Key::RebuildWidget()
      AddDigitButton(Grid, 7, 0, 0);
      AddDigitButton(Grid, 8, 0, 1);
      AddDigitButton(Grid, 9, 0, 2);
+     AddOpButton(Grid, 1, 0, 3);
 
      AddDigitButton(Grid, 4, 1, 0);
      AddDigitButton(Grid, 5, 1, 1);
      AddDigitButton(Grid, 6, 1, 2);
+     AddOpButton(Grid, 2, 1, 3);
 
      AddDigitButton(Grid, 1, 2, 0);
      AddDigitButton(Grid, 2, 2, 1);
      AddDigitButton(Grid, 3, 2, 2);
+     AddOpButton(Grid, 3, 2, 3);
 
-     AddDigitButton(Grid, 0, 3, 1);
+     AddDigitButton(Grid, 0, 3, 0);
+     AddOpButton(Grid, 5, 3, 1);
+     AddOpButton(Grid, 6, 3, 2);
+     AddOpButton(Grid, 4, 3, 3);
 
      AddClearButton(Grid, 3, 2);
 
@@ -89,6 +95,43 @@ FReply UBasic10Key::OnClearClicked()
 {
      ClearPressed.Broadcast();
      return FReply::Handled();
+}
+
+void UBasic10Key::AddOpButton(
+     const TSharedRef<SUniformGridPanel>& Grid,
+     uint8 OpCode,
+     int32 Row,
+     int32 Col)
+{
+     Grid->AddSlot(Col, Row)
+     [
+           SNew(SButton)
+           .OnClicked(FOnClicked::CreateUObject(this, &UBasic10Key::OnOpClicked, OpCode)
+           [
+               FString operation switch(OpCode)
+               {
+                    1 => "+",
+                    2 => "-",
+                    3 => "*",
+                    4 => "/",
+                    5 => ".",
+                    6 => "=",
+
+                    _ => ""
+               };
+
+               SNew(STextBlock)
+               .Text(FText::FromString(operation)
+               .Justification(ETextJustify::Center)
+               .Font(FSlateFontInfo("Roboto", 32))
+           ]
+     ];
+}
+
+FReply UBasic10Key::OnOpClicked(uint8 OpCode)
+{
+     OpPressed.Broadcast(OpCode);
+     return FReply::Handled;
 }
 
 // EOF
